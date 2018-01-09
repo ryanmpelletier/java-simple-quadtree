ab3bb0e02a8e8f46c606f1fcad28d130b6d65712
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2018-01-08 19:09:02 -0500

    will use this later

---
+++ src/test/java/com/pelletier/PerformanceReport.java
@@ -0,0 +1,20 @@
+package com.pelletier;
+
+import java.util.Random;
+
+public class PerformanceReport {
+	
+	
+	
+	public static void main(String[] args){
+	
+		/**
+		 * A really big quadtree, can hold 25 objects per tree,
+		 * will go up to 10 levels deep, starts at (0,0) and is 10,000 * 10,000, no parent
+		 */
+//		QuadTree quadTree = new QuadTree(25, 10, 0, 0, 0, 10000, 10000, null);
+		
+		//will read from file and put 10,000 random 10*10 objects into the quadtree
+		
+	}
+}
