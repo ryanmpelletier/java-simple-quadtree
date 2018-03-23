249dedb758b5dbfcf40e014f0f6f3ff8a40e93f7
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2018-03-23 18:12:17 -0400

    Added more comments and refactored the Performance report to clearly show performance between QuadTree and brute force.

--- src/test/java/com/pelletier/Box.java
+++ src/test/java/com/pelletier/Box.java
@@ -7,19 +7,27 @@ import com.pelletier.geometry.GeometryUtil;
 import com.pelletier.geometry.RectangleObject;
 import com.pelletier.geometry.SearchRectangleObject;
 
+/**
+ * This class is a container to test the QuadTree against. It simply keeps a list of RectangleObjects,
+ * and searches by comparing every object in the list to the search area it is a O(n*n) solution to collision detection.
+ */
 public class Box {
-	
-	public List<RectangleObject> rectangleObjects;
+
+    //list of items this container is holding
+    public List<RectangleObject> rectangleObjects;
 	
 	public Box(){}
-	
+
+	//adds an item to this container
     public void insert(RectangleObject rectangleObject){
     	if(this.rectangleObjects == null){
     		this.rectangleObjects = new ArrayList<>();
     	}
     	this.rectangleObjects.add(rectangleObject);
     }
-    
+
+
+    //searches for items in this container that overlap the search area
     public List<RectangleObject> search(SearchRectangleObject searchRectangleObject){
     	List<RectangleObject> rectangleObjects = new ArrayList<>();
 		for(RectangleObject objectInBox: this.rectangleObjects){
--- src/test/java/com/pelletier/PerformanceReport.java
+++ src/test/java/com/pelletier/PerformanceReport.java
@@ -11,6 +11,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+
+/**
+ * Compares a brute force 2d collision detection method vs the QuadTree.
+ * Inserts 10,000 10 * 10 objects at randomly generated coordinates.
+ * Compares the amount of time it takes to query both the Box (brute force) and the QuadTree.
+ */
 public class PerformanceReport {
 	
 	
@@ -18,17 +24,24 @@ public class PerformanceReport {
 	public static void main(String[] args){
 	
 		/**
-		 * A really big quadtree, can hold 25 objects per tree,
-		 * will go up to 10 levels deep, starts at (0,0) and is 10,000 * 10,000, no parent
+		 * A 10,000 * 10,000 QuadTree. It will expand to at most 3 levels, and will carry a maximum of 25
+		 * objects per node before splitting, unless the maxLevels has been reached.
 		 */
 		QuadTree quadTree = new QuadTree(25, 3, 0, 0, 0, 10000, 10000, null);
+
+		/**
+		 * A simple container for storing objects.
+		 * It doesn't need size bounds because it just stores a list of objects.
+		 */
 		Box box = new Box();
 
-		
-		//will read from file and put 10,000 random 10*10 objects into the quadtree
+        /**
+         * objectCoordinates.txt contains 10,000 pairs of randomly generated coordinates
+         * A 10 * 10 object will be inserted into both the Box and the QuadTree at each of these coordinates.
+         */
 		
 		try {
-			List<String> lines = Files.readAllLines(Paths.get("src/test/resources/objects.txt"));
+			List<String> lines = Files.readAllLines(Paths.get("src/test/resources/objectCoordinates.txt"));
 						
 			for(int i = 0; i < lines.size(); i++){
 				box.insert(new SearchRectangleObject(Double.parseDouble(lines.get(i).split(" ")[0]), Double.parseDouble(lines.get(i).split(" ")[1]), 10.0, 10.0, String.valueOf(i)));
@@ -36,36 +49,68 @@ public class PerformanceReport {
 			}
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		
 		System.out.println("***** Quadtree Summary *****");
 		System.out.println("Total Objects: " + quadTree.getTotalObjects());
-		System.out.println("Depth: " + quadTree.getDepth());
+		System.out.println("Actual Depth: " + quadTree.getDepth());
+        System.out.println("Configured Max Levels: " + quadTree.getMaxLevels());
+        System.out.println("Configured Max Objects Per Node: " + quadTree.getMaxObjects());
 		System.out.println("******************************************");
-		
-		int numberOfObjectsQueriedFor = 0;
+		System.out.println("\n");
+
+        System.out.println("***** Box Summary *****");
+        System.out.println("Total Objects: " + box.rectangleObjects.size());
+        System.out.println("***********************");
+        System.out.println("\n");
+
+
+        /**
+         * Stat variables for the Box and QuadTree.
+         */
+        int quadTreeTotalObjectsQueriedFor = 0;
+        long quadTreeTotalQueryTime = 0;
+        long quadTreeMaxQueryTime = 0;
+
+        int boxTotalObjectsQueriedFor = 0;
+        long boxTotalQueryTime = 0;
+        long boxMaxQueryTime = 0;
+
+
 		try {
 			List<String> lines = Files.readAllLines(Paths.get("src/test/resources/randomSearchAreas.txt"));
-			
-			//can't do parseInt here, will screw up results,should buffer all numbers into an array
+
+            /**
+             * I am going to create a bunch of SearchRectangleObjects now so that I don't need to later.
+             * I don't want it to affect the query time.
+             */
 			SearchRectangleObject[] searchRectangleObjects = new SearchRectangleObject[lines.size()];
-			
 			for(int i = 0; i < lines.size(); i++){
 				searchRectangleObjects[i] = new SearchRectangleObject(Double.parseDouble(lines.get(i).split(" ")[0]), Double.parseDouble(lines.get(i).split(" ")[1]), 
 						Double.parseDouble(lines.get(i).split(" ")[2]), Double.parseDouble(lines.get(i).split(" ")[3]));
 			}
 			
-			System.out.println("***** Beginning Searches *****");
-			long startMilliseconds = System.currentTimeMillis();
-			System.out.println("Time: "  + new Date(startMilliseconds));
+			System.out.println("***** Beginning Searches *****\n");
+
 			
 			for(int i = 0; i < searchRectangleObjects.length; i++){
+
+			    long boxSearchStartTime = System.currentTimeMillis();
 				List<RectangleObject> itemsFromBox = box.search(searchRectangleObjects[i]);
+				long now = System.currentTimeMillis();
+				long boxQueryTime = now - boxSearchStartTime;
+				boxTotalQueryTime = boxTotalQueryTime + boxQueryTime;
+				boxMaxQueryTime = Math.max(boxMaxQueryTime, boxQueryTime);
+
+				long quadTreeSearchStartTime = System.currentTimeMillis();
 				List<RectangleObject> itemsFromQuadTree = quadTree.search(searchRectangleObjects[i]);
-				
+				now = System.currentTimeMillis();
+				long quadTreeQueryTime = now - quadTreeSearchStartTime;
+				quadTreeTotalQueryTime = quadTreeTotalQueryTime + quadTreeQueryTime;
+				quadTreeMaxQueryTime = Math.max(quadTreeMaxQueryTime, quadTreeQueryTime);
+
 				if(itemsFromBox.size() != itemsFromQuadTree.size()){
 					System.out.println("Search Area-> " + " X:" + searchRectangleObjects[i].getX() + " Y: " + searchRectangleObjects[i].getY() + " W: " + searchRectangleObjects[i].getW() + " H: " + searchRectangleObjects[i].getH());
 					List<RectangleObject> differentObjects = getDifferentObjects(itemsFromBox, itemsFromQuadTree);
@@ -73,21 +118,33 @@ public class PerformanceReport {
 					for(int j = 0; j < differentObjects.size(); j++){
 						System.out.println(" X:" + differentObjects.get(j).getX() + " Y: " + differentObjects.get(j).getY() + " W: " + differentObjects.get(j).getW() + " H: " + differentObjects.get(j).getH());
 					}
+					throw new RuntimeException("Identical Query for Box and QuadTree returned different objects!");
 				}
-				
-				numberOfObjectsQueriedFor += itemsFromQuadTree.size();
-//				System.out.println((i + 1) + ". " + lines.get(i).split(" ")[0] + " " + lines.get(i).split(" ")[1] + " " + lines.get(i).split(" ")[2] + " " + lines.get(i).split(" ")[3] + " " + items.size());
-			}
-			long endMilliseconds = System.currentTimeMillis();
 
-			System.out.println("***** Searches Complete *****");
-			System.out.println("Total Searches: " + searchRectangleObjects.length);
-			System.out.println("Total Objects Queried For: " + numberOfObjectsQueriedFor);
-			System.out.println("Time Elapsed (millis): " + (endMilliseconds - startMilliseconds));
-			
-			
+                quadTreeTotalObjectsQueriedFor = quadTreeTotalObjectsQueriedFor + itemsFromQuadTree.size();
+				boxTotalObjectsQueriedFor = boxTotalObjectsQueriedFor + itemsFromBox.size();
+			}
+
+
+            System.out.println("***** QuadTree Performance Summary *****");
+            System.out.println("Total Queries Run: " + searchRectangleObjects.length);
+            System.out.println("Total Objects Queried For: " + quadTreeTotalObjectsQueriedFor);
+            System.out.println("Total Query Time: " + quadTreeTotalQueryTime);
+            System.out.println("Max Query Time: " + quadTreeMaxQueryTime);
+            System.out.println("Average Query Time: " + ((float) quadTreeTotalQueryTime / (float) searchRectangleObjects.length));
+            System.out.println("\n");
+
+            System.out.println("***** Box Performance Summary *****");
+            System.out.println("Total Queries Run: " + searchRectangleObjects.length);
+            System.out.println("Total Objects Queried For: " + boxTotalObjectsQueriedFor);
+            System.out.println("Total Query Time: " + boxTotalQueryTime);
+            System.out.println("Max Query Time: " + boxMaxQueryTime);
+            System.out.println("Average Query Time: " + ((float)boxTotalQueryTime / (float)searchRectangleObjects.length));
+            System.out.println("\n");
+
+
+
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
